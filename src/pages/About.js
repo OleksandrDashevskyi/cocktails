64203491e1fc4084784c762ca151caaa19dcd25d
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-12-18 00:02:18 -0800

    cocktails

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum eius error id illo nulla quasi
+                recusandae similique voluptate voluptatibus! Aut consectetur cumque harum laudantium officiis qui quos
+                unde? Quam?
+            </p>
+        </section>
+    );
+}
+
+export default About;
